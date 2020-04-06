b88a5bee4a917e0b6c8ae2f8fc311906ac2cde6b
wangjirong <408718358@qq.com>
2020-04-06 14:20:48 +0800

    删除多余

--- src/getLocaltionBrowser.js
+++ src/getLocaltionBrowser.js
@@ -1,24 +1,3 @@
-//获得年月日
-export function getYearMonDay(date) {
-    const time = new Date(date);
-    const year = time.getFullYear();
-    let month = time.getMonth() + 1;
-    month = month > 10 ? month : "0" + month;
-    let day = time.getDate();
-    day = day > 10 ? day : "0" + day;
-    return `${year}-${month}-${day}`;
-};
-//获得具体时间
-export function getDetailTime(date) {
-    const time = new Date(date);
-    let hours = time.getHours();
-    hours = hours > 10 ? hours : "0" + hours;
-    let minutes = time.getMinutes();
-    minutes = minutes > 10 ? minutes : "0" + minutes;
-    let seconds = time.getSeconds();
-    seconds = seconds > 10 ? seconds : "0" + seconds;
-    return `${hours}:${minutes}:${seconds}`;
-}
 //获取浏览器类型
 export function getBrowerType() {
     const userAgent = window.navigator.userAgent.toLowerCase();
